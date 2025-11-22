bdb86d429cf599dc01105bff4874c7eb4265eee8
PierreBond <cyberkeen@proton.me>
2025-11-22 01:10:59 +0100

    bug fie 1

--- blockchain.py
+++ blockchain.py
@@ -149,7 +149,7 @@ class Blockchain(object):
         # :return: <str>
 
         #we must make sure that the dictionary is ordered , or we'll have inconsistent hashes
-        block_string = json.dump(block, sort_keys=True).encode()
+        block_string = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest()
 
     def proof_of_work(self, last_proof):
